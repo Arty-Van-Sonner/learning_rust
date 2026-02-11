d72242a12ec73b6128e129d72590fae1340574c4
Arty-Van-Sonner <valter.199669@gmail.com>
2026-02-11 22:33:45 +0300

    Variables in Rust

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # learning_rust
+
+## Commands
+1. rustc src/main.rs
+2. cargo new project_name
+3. cargo run
+4. cargo build
+5. rustc --version
+6. cargo check
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
+    // comment
+
+    /* 
+        Multi-line
+        comment
+    */ 
     println!("Hello, world!");
+
+    // Integer: i8; i16; i32; i64; i128; u8; u16; u32; u64; u128
+    let mut num: u8 = 50;
+    num += 10;
+    println!("Result: {}", num);
+
+    let num = "hello";
+    println!("Result: {}", num);
+
+    // Float
+    let num: f32 = 5.634;
+    println!("Result: {}", num);
+
+    // Boolean
+    let num: bool = true;
+    println!("Result: {}", num);
+
+    // Char
+    let sym: char = '%';
+    println!("Result: {}", sym);
 }
